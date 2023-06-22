591acffb38208946008de346de43804d9fcf227c
LeeKokHong <LeeKokHong@gmail.com>
2023-06-22 11:14:38 +0800

    Initial Commit

---
+++ SourceControl/SourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlApp.swift
+//  SourceControl
+//
+//  Created by Lee Kok Hong on 22/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
